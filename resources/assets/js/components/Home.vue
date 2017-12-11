10bfce7bba59a6fbf559c9155dc2e40579788784
kinross long <longkinross@gmail.com>
2017-12-11 14:51:09 +0800

    二次提交

--- resources/assets/js/components/Home.vue
+++ resources/assets/js/components/Home.vue
@@ -139,6 +139,11 @@
                             </el-form-item>
                         </el-form>
 
+                        <!--MessageBox 弹框 确认消息 -->
+                        <template>
+                            <el-button type="text" @click="MessageBox">点击打开 Message Box</el-button>
+                        </template>
+
                         <!-- Element Dialog对话框-->
                         <el-dialog title="Post 编辑" :visible.sync="dialogFormVisible"  width="40%">
                             <el-form :model="dialogEdit">
@@ -262,6 +267,23 @@
             edit() {
                 this.dialogFormVisible = false;
                 console.log();
+            },
+            MessageBox() { //弹框
+                this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', {
+                confirmButtonText: '确定',
+                cancelButtonText: '取消',
+                type: 'warning'
+                }).then(() => {
+                this.$message({
+                    type: 'success',
+                    message: '删除成功!'
+                });
+                }).catch(() => {
+                this.$message({
+                    type: 'info',
+                    message: '已取消删除'
+                });          
+                });
             }
         }
 
