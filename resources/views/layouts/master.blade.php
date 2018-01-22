dc18970a480160b5d32137990bbfaea7fdf2ed4a
kinross long <longkinross@gmail.com>
2018-01-22 19:19:19 +0800

    布局文件修改分拆js成多个js文件

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,6 +5,7 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>Laravel Vue</title>
         <link rel="stylesheet" href="/css/app.css">
         {{--  <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico">  --}}
@@ -14,6 +15,8 @@
         <div id="app">
             <router-view></router-view>
         </div>
-        <script src="/js/app.js"></script>
+        <script src="{{ mix('js/manifest.js') }}"></script>
+        <script src="{{ mix('js/vendor.js') }}"></script>
+        <script src="{{ mix('js/app.js') }}"></script>
     </body>
 </html>
