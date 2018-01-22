693dc129e4dbfbdb13aff8e792508b58d5b9309c
kinross long <longkinross@gmail.com>
2018-01-22 19:27:43 +0800

    修改控制器请求

--- app/Post.php
+++ app/Post.php
@@ -5,6 +5,7 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
-{
+{   
+    // protected $hidden = ['user_id','title'];
     protected $fillable = ['title', 'body', 'user_id'];
 }
